3617111c6cc978a484ef65cf865113553e06fc66
Yonas Negash <yonas.negash91@gmail.com>
2024-06-21 16:53:25 +0300

    Initial commit - basic backend setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is up and listening on port ${PORT}`);
+});
